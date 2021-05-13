bf2ad4cfddd33476595921edfa0cbb6124a5b3c0
xsmorex <banasikannie@gmail.com>
2021-05-13 03:22:28 +0100

    Add loading content

---
+++ src/Shared/Content.js
@@ -0,0 +1,14 @@
+import {AppContext} from "../App/AppProvider";
+
+export default function (props){
+  return <AppContext.Consumer>
+    {({coinList}) =>{
+      if(!coinList){
+        return <div>
+          Loading Coins
+        </div>
+      }
+      return <div>{props.children}</div>
+    }}
+  </AppContext.Consumer>
+}
